13318a9d30126b9c7061d01f855d0356415ba4a9
Victor Agu <v.ebukaagu@gmail.com>
2025-01-08 12:25:47 +0100

    edited footer (2)

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <p>&copy; 2025 Victor Agu. All Rights Reserved.</p>
       <div className="social-links">
         <a href="https://github.com/Victor-Ebuka">GitHub</a>
-        <a href="www.linkedin.com/in/victor-agu-ebuka">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/victor-agu-ebuka">LinkedIn</a>
       </div>
     </footer>
   );
